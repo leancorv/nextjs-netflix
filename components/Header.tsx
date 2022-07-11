f4a36acbd7f3b8b71886dbb48fc50f60149c0d1d
Leandro <lecorvalan2@gmail.com>
2022-07-11 16:10:33 -0300

    Estilos del Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 import {BellIcon, SearchIcon} from "@heroicons/react/solid"
+import Link from "next/link"
 
 function Header() {
   return (
@@ -20,10 +21,17 @@ function Header() {
             </ul>
         </div>
 
-        <div>
+        <div className="flex items-center space-x-4 text-sm font-light">
             <SearchIcon className="hidden h-6 w-6 sm:inline"/>
             <p className="hidden lg:inline">Kids</p>
             <BellIcon className="h-6 w-6"/>
+            <Link href="/account">
+                <img
+                src="https://rb.gy/g1pwyx"
+                alt=""
+                className="cursor-pointer rounded"
+                />
+            </Link>
         </div>
     </header>
   )
