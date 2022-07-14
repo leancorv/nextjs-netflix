11c3918d3bc5b3710b513f23093ab5be1e4c05a8
Leandro <lecorvalan2@gmail.com>
2022-07-14 01:23:01 -0300

    Importando AuthProvider

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,6 +2,7 @@ import Head from "next/head"
 import Image from "next/image"
 import { useState } from "react"
 import { SubmitHandler, useForm } from "react-hook-form"
+import useAuth from "../hooks/useAuth"
 
 interface Inputs {
     email: string
@@ -10,6 +11,8 @@ interface Inputs {
 
 function login() {
     const [login, setLogin] = useState(false)
+    const { signIn, signUp} = useAuth()
+
     const { register, handleSubmit, formState: { errors } } = useForm<Inputs>();
 
     const onSubmit: SubmitHandler<Inputs> = async ({email, password}) => {
