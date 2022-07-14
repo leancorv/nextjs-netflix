6ec46ed09b33b30344da3df7d7ab1df5ec4e946b
Leandro <lecorvalan2@gmail.com>
2022-07-14 17:15:26 -0300

    Creando modalButton

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,3 +1,4 @@
+import { XIcon } from "@heroicons/react/outline"
 import MuiModal from "@mui/material/Modal"
 import { useRecoilState } from "recoil"
 import { modalState } from "../atoms/modalAtom"
@@ -11,7 +12,12 @@ function Modal() {
   return (
     <MuiModal open={showModal} onClose={handleClose}>
         <>
-            Modal
+            <button 
+                onClick={handleClose} 
+                className="modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]"
+            >
+                <XIcon className="h-6 w-6"/>
+            </button>
         </>
     </MuiModal>
   )
