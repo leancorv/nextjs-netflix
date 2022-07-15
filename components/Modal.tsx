c0c2a7fa2a9621a19bcecff57f16501152cac577
Leandro <lecorvalan2@gmail.com>
2022-07-15 00:39:52 -0300

    Haciendo el fetch de movie/tv trailer de la api

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -2,19 +2,43 @@ import { XIcon } from "@heroicons/react/outline"
 import MuiModal from "@mui/material/Modal"
 import { useEffect, useState } from "react"
 import { useRecoilState } from "recoil"
-import { modalState } from "../atoms/modalAtom"
-import { Movie } from "../typings"
+import { modalState, movieState } from "../atoms/modalAtom"
+import {Element, Genre} from "../typings"
 
 function Modal() {
     const [ showModal, setShowModal] = useRecoilState(modalState)
-    const [movie, setMovie] = useState<Movie | null>(null)
+    const [movie, setMovie] = useRecoilState(movieState)
+    const [data, setData] = useState()
+    const [trailer, setTrailer] = useState("")
+    const [genres, setGenres] = useState<Genre[]>()
 
     useEffect(() => {
         if(!movie) return 
 
+        async function fetchMovie() {
+            const data = await fetch(
+                `https://api.themoviedb.org/3/${
+                  movie?.media_type === 'tv' ? 'tv' : 'movie'
+                }/${movie?.id}?api_key=${
+                  process.env.NEXT_PUBLIC_API_KEY
+                }&language=en-US&append_to_response=videos`
+            )
+            .then((response) => response.json())
+            .catch((err) => console.log(err.message))
 
-    }, [])
-    
+            if (data?.videos) {
+                const index = data.videos.results.findIndex(
+                    (element: Element) => element.type === "Trailer"
+                )
+                setTrailer(data.videos?.results[index]?.key)
+            }
+            if (data?.genres) {
+                setGenres(data.genres)
+            }
+        }
+
+        fetchMovie()
+    }, [movie])
 
     const handleClose = () => {
         setShowModal(false)
